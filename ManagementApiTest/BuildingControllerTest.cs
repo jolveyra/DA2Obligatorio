9c5efc97a9d393e846046f5343b6ad09869fe261
jppuig <jppuigpesce@gmail.com>
2024-04-10 09:54:27 -0300

    REFACTOR: GetAllBuildingsTestOk

--- ManagementApiTest/BuildingControllerTest.cs
+++ ManagementApiTest/BuildingControllerTest.cs
@@ -13,30 +13,21 @@ namespace ManagementApiTest
         [TestMethod]
         public void GetAllBuildingsTestOk()
         {
-            IEnumerable<Building> _buildings = new List<Building>
-            {
-                new Building() { Name = "Mirador" }
-            };
+            IEnumerable<Building> buildings = new List<Building> { new Building() { Name = "Mirador" } };
 
-            Mock<IBuildingLogic> buildingLogicMock = new Mock<IBuildingLogic>();
-            buildingLogicMock.Setup(x => x.GetAllBuildings()).Returns(_buildings);
+            Mock<IBuildingLogic> buildingLogicMock = new Mock<IBuildingLogic>(MockBehavior.Strict);
+            buildingLogicMock.Setup(x => x.GetAllBuildings()).Returns(buildings);
 
-            BuildingController _buildingController = new BuildingController(buildingLogicMock.Object);
+            BuildingController buildingController = new BuildingController(buildingLogicMock.Object);
 
-            OkObjectResult expected = new OkObjectResult(new List<BuildingResponseModel>
-            {
-                new BuildingResponseModel(_buildings.First())
-            });
-
+            OkObjectResult expected = new OkObjectResult(new List<BuildingResponseModel> { new BuildingResponseModel(buildings.First()) });
             List<BuildingResponseModel> expectedObject = expected.Value as List<BuildingResponseModel>;
 
-            OkObjectResult result = _buildingController.GetAllBuildings() as OkObjectResult;
-            Console.WriteLine(result.Value);
+            OkObjectResult result = buildingController.GetAllBuildings() as OkObjectResult;
+            List<BuildingResponseModel> objectResult = result.Value as List<BuildingResponseModel>;
 
-            List<BuildingResponseModel> ObjectResult = result.Value as List<BuildingResponseModel>;
-
-            Assert.IsTrue(expected.StatusCode.Equals(result.StatusCode));
-
+            buildingLogicMock.VerifyAll();
+            Assert.IsTrue(expected.StatusCode.Equals(result.StatusCode) && expectedObject.First ().Name.Equals(objectResult.First().Name));
         }
     }
 }
