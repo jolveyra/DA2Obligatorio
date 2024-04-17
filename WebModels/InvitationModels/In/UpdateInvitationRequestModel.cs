b35981bfa09984ea4aa115a8cdbf93eadd5a2c8d
jppuig <jppuigpesce@gmail.com>
2024-04-17 18:21:30 -0300

    REFACTOR: UpdateInvitationByIdTestOk

--- WebModels/InvitationModels/In/UpdateInvitationRequestModel.cs
+++ WebModels/InvitationModels/In/UpdateInvitationRequestModel.cs
@@ -2,6 +2,16 @@
 {
     public class UpdateInvitationRequestModel
     {
-        public bool IsAccepted { get; set; }
+        public bool IsAccepted
+        {
+            get
+            {
+                if (IsAccepted == null)
+                    throw new MissingFieldException("There is a missing field in the request's body");
+
+                return IsAccepted;
+            }
+            set => IsAccepted = value;
+        }
     }
 }
