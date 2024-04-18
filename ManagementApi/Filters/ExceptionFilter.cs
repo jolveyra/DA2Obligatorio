aa6671b5b73f8005897491ae52631ee324079328
jppuig <jppuigpesce@gmail.com>
2024-04-18 14:49:10 -0300

    Correct exceptionFilter

--- ManagementApi/Filters/ExceptionFilter.cs
+++ ManagementApi/Filters/ExceptionFilter.cs
@@ -10,12 +10,12 @@ namespace ManagementApi.Filters
             if (context.Exception is MissingFieldException)
             {
                 context.Result = new ObjectResult(new { ErrorMessage = context.Exception.Message }) { StatusCode = 400 };
+            } else if (context.Exception is InvalidOperationException) 
+            {
+                context.Result = new ObjectResult(new { ErrorMessage = context.Exception.Message }) { StatusCode = 400 };
             } else if (context.Exception is ArgumentException)
             {
                 context.Result = new ObjectResult(new { ErrorMessage = context.Exception.Message }) { StatusCode = 404 };
-            } else if (context.Exception is InvalidOperationException) 
-            {
-                context.Result = new ObjectResult(new { ErrorMessage = context.Exception.Message }) { StatusCode = 409 };
             } else if (context.Exception is Exception)
             {
                 context.Result = new ObjectResult(new { ErrorMessage = $"Something went wrong. See: {context.Exception.Message}" }) { StatusCode = 500 };
