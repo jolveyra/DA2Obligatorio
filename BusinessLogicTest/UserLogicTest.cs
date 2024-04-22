6e9350f9ba043ec79ca72910407785a9f5daafe2
jppuig <jppuigpesce@gmail.com>
2024-04-22 14:16:01 -0300

    REFACTOR: CreateUserWithExistingEmailTest

--- BusinessLogicTest/UserLogicTest.cs
+++ BusinessLogicTest/UserLogicTest.cs
@@ -91,9 +91,9 @@ namespace BusinessLogicTest
         public void CreateUserWithExistingEmailTest()
         {
             User user = new User { Name = "Juan", Surname = "Perez", Email = "juan@gmail.com", Password = "Juan1234", Role = Role.Administrator };
-            
-            // CAMBIAR
-            userRepositoryMock.Setup(u => u.GetAllUsers()).Throws(new UserException("A user with the same email already exists"));
+            IEnumerable<User> users = new List<User> { user };
+
+            userRepositoryMock.Setup(u => u.GetAllUsers()).Returns(users);
             Exception exception = null;
 
             try
