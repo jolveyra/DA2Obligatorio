982ff3f065a15738b2b8c3da41f03992e54c6c6a
jppuig <jppuigpesce@gmail.com>
2024-04-26 19:39:53 -0300

    REFACTOR: CreateSessionTest

--- DataAccessTest/SessionRepositoryTest.cs
+++ DataAccessTest/SessionRepositoryTest.cs
@@ -9,17 +9,25 @@ namespace DataAccessTest
     [TestClass]
     public class SessionRepositoryTest
     {
+        private Mock<BuildingBossContext> _contextMock;
+        private SessionRepository _sessionRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _contextMock = new Mock<BuildingBossContext>();
+            _sessionRepository = new SessionRepository(_contextMock.Object);
+        }
+
         [TestMethod]
         public void GetUserIdByTokenTest()
         {
             Guid token = Guid.NewGuid();
             Session session = new Session { Id = token, UserId = Guid.NewGuid() };
 
-            Mock<BuildingBossContext> _contextMock = new Mock<BuildingBossContext>();
             _contextMock.Setup(context => context.Sessions).ReturnsDbSet(new List<Session>() { session });
-            SessionRepository sessionRepository = new SessionRepository(_contextMock.Object);
-
-            Guid userId = sessionRepository.GetUserIdByToken(token);
+            
+            Guid userId = _sessionRepository.GetUserIdByToken(token);
 
             _contextMock.Verify(context => context.Sessions, Times.Once);
             Assert.AreEqual(session.UserId, userId);
@@ -30,12 +38,10 @@ namespace DataAccessTest
         {
             Session session = new Session { Id = Guid.NewGuid(), UserId = Guid.NewGuid() };
 
-            Mock<BuildingBossContext> _contextMock = new Mock<BuildingBossContext>();
             _contextMock.Setup(context => context.Sessions.Add(session));
             _contextMock.Setup(context => context.SaveChanges()).Returns(1);
-            SessionRepository sessionRepository = new SessionRepository(_contextMock.Object);
-
-            Session newSession = sessionRepository.CreateSession(session);
+            
+            Session newSession = _sessionRepository.CreateSession(session);
 
             _contextMock.Verify(context => context.Sessions.Add(session), Times.Once);
             _contextMock.Verify(context => context.SaveChanges(), Times.Once);
