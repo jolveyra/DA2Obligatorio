f11659a002709f10eb306ddc05aa219129f71dfb
jppuig <jppuigpesce@gmail.com>
2024-04-26 16:53:16 -0300

    GREEN: CreateExistingIdInvitationTest

--- DataAccessTest/InvitationRepositoryTest.cs
+++ DataAccessTest/InvitationRepositoryTest.cs
@@ -80,14 +80,41 @@ namespace DataAccessTest
         {
             Invitation invitation = new Invitation() { Id = Guid.NewGuid(), Name = "Juan", Email = "juan@gmail.com" };
 
+            _contextMock.Setup(context => context.Invitations).ReturnsDbSet(new List<Invitation>());
             _contextMock.Setup(context => context.Invitations.Add(invitation));
             _contextMock.Setup(context => context.SaveChanges()).Returns(1);
 
             Invitation result = _invitationRepository.CreateInvitation(invitation);
 
+            _contextMock.Verify(context => context.Invitations, Times.Exactly(2));
             _contextMock.Verify(context => context.Invitations.Add(invitation), Times.Once());
             _contextMock.Verify(context => context.SaveChanges(), Times.Once());
             Assert.AreEqual(invitation, result);
+        }
+
+        [TestMethod]
+        public void CreateExistingIdInvitationTest()
+        {
+            Guid id = Guid.NewGuid();
+            Invitation invitation = new Invitation() { Id = id, Name = "Juan", Email = "juan@gmail.com" };
+            _contextMock.Setup(context => context.Invitations).ReturnsDbSet(new List<Invitation>()
+            {
+                new Invitation() { Id = id, Name = "Jose", Email = "jose@gmail.com" }
+            });
+            Exception exception = null;
+
+            try
+            {
+                _invitationRepository.CreateInvitation(invitation);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            _contextMock.Verify(context => context.Invitations, Times.Once());
+            Assert.IsInstanceOfType(exception, typeof(ArgumentException));
+            Assert.AreEqual("An invitation with the same id already exists", exception.Message);
         }
     }
 }
