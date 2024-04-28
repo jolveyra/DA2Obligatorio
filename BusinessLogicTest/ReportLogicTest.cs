d65202e8363158e4b4a7adc9d2de5e035ee00828
jppuig <jppuigpesce@gmail.com>
2024-04-28 20:37:31 -0300

    GREEN: GetBuildingReportTest

--- BusinessLogicTest/ReportLogicTest.cs
+++ BusinessLogicTest/ReportLogicTest.cs
@@ -32,5 +32,86 @@ namespace BusinessLogicTest
             Assert.IsInstanceOfType(exception, typeof(ReportException));
             Assert.AreEqual("Invalid filter", exception.Message);
         }
+
+        [TestMethod]
+        public void GetBuildingReportTest()
+        {
+            string filter = "Building";
+
+            Mock<IRequestRepository> requestRepositoryMock = new Mock<IRequestRepository>();
+            ReportLogic reportLogic = new ReportLogic(requestRepositoryMock.Object);
+
+            List<Request> requests = new List<Request>
+            {
+                new Request
+                {
+                    Flat = new Flat
+                    {
+                        Building = new Building
+                        {
+                            Name = "Building1"
+                        }
+                    }
+                },
+                new Request
+                {
+                    Flat = new Flat
+                    {
+                        Building = new Building
+                        {
+                            Name = "Building2"
+                        }
+                    }
+                },
+                new Request
+                {
+                    Flat = new Flat
+                    {
+                        Building = new Building
+                        {
+                            Name = "Building1"
+                        }
+                    }
+                },
+                new Request
+                {
+                    Flat = new Flat
+                    {
+                        Building = new Building
+                        {
+                            Name = "Building2"
+                        }
+                    },
+                    Status = RequestStatus.Completed,
+                    StartingDate = DateTime.Now.AddHours(-2),
+                    CompletionDate = DateTime.Now
+                },
+                new Request
+                {
+                    Flat = new Flat
+                    {
+                        Building = new Building
+                        {
+                            Name = "Building1"
+                        }
+                    },
+                    Status = RequestStatus.InProgress,
+                    StartingDate = DateTime.Now.AddHours(-2)
+                }
+            };
+
+            requestRepositoryMock.Setup(r => r.GetAllRequests()).Returns(requests);
+
+            List<(string, int, int, int, double)> expectedReport = new List<(string, int, int, int, double)>
+            {
+                ("Building1", 2, 1, 0, 0),
+                ("Building2", 1, 0, 1, 2)
+            };
+
+            List<(string, int, int, int, double)> actualReport = reportLogic.GetReport(filter);
+
+            requestRepositoryMock.VerifyAll();
+            CollectionAssert.AreEqual(expectedReport, actualReport);
+        }
     }
 }
