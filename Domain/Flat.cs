ac60408ee52f3cc0225e09647c657a3121904ac7
Javier Olveyra <jolveyra@gmail.com>
2024-04-30 01:04:59 -0300

    FIX: Added default values in flat

--- Domain/Flat.cs
+++ Domain/Flat.cs
@@ -4,12 +4,12 @@
     {
         public Guid Id { get; set; }
         public Building Building { get; set; }
-        public int Number { get; set; }
-        public int Floor { get; set; }
-        public string OwnerName { get; set; }
-        public string OwnerSurname { get; set; }
-        public string OwnerEmail { get; set; }
-        public int Bathrooms { get; set; }
-        public bool HasBalcony { get; set; }
+        public int Number { get; set; } = 0;
+        public int Floor { get; set; } = 0;
+        public string OwnerName { get; set; } = "";
+        public string OwnerSurname { get; set; } = "";
+        public string OwnerEmail { get; set; } = "";
+        public int Bathrooms { get; set; } = 0;
+        public bool HasBalcony { get; set; } = false;
     }
 }
