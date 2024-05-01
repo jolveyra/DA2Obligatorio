c872b0cc6767a888b91a15ea6299e3b0f59e4b85
jppuig <jppuigpesce@gmail.com>
2024-05-01 11:58:25 -0300

    REFACTOR: report

--- BusinessLogic/Report/RequestReport.cs
+++ BusinessLogic/Report/RequestReport.cs
@@ -22,39 +22,78 @@ namespace BusinessLogic
 
             foreach (List<Request> requestList in requestsPerFilter)
             {
-                int pending = 0;
-                int inProgress = 0;
-                int completed = 0;
-                double avgTimeToComplete = 0;
-
-                foreach (Request request in requestList)
-                {
-                    if (request.Status == RequestStatus.Pending)
-                    {
-                        pending++;
-                    }
-                    
-                    if (request.Status == RequestStatus.InProgress)
-                    {
-                        inProgress++;
-                    }
-
-                    if (request.Status == RequestStatus.Completed)
-                    {
-                        avgTimeToComplete += (request.CompletionDate - request.StartingDate).Hours;
-                        completed++;
-                    }
-                }
-                
-                if (completed > 0)
-                {
-                    avgTimeToComplete /= completed;
-                }
-
-                requestTuples.Add((pending, inProgress, completed, avgTimeToComplete));
+                IterateThroughListOfRequestByFilter(requestList, ref requestTuples);
             }
 
             return requestTuples;
+        }
+
+        private static void IterateThroughListOfRequestByFilter(List<Request> requestList, ref List<(int, int, int, double)> requestTuples)
+        {
+            int pending = 0;
+            int inProgress = 0;
+            int completed = 0;
+            double avgTimeToComplete = 0;
+
+            foreach (Request request in requestList)
+            {
+                pending = IterateThroughRequests(request, ref pending, ref inProgress, ref avgTimeToComplete, ref completed);
+            }
+                
+            if (completed > 0)
+            {
+                avgTimeToComplete = CalculateAverageTimeOfCompletion(avgTimeToComplete, completed);
+            }
+
+            requestTuples.Add((pending, inProgress, completed, avgTimeToComplete));
+        }
+
+        private static double CalculateAverageTimeOfCompletion(double avgTimeToComplete, int completed)
+        {
+            avgTimeToComplete /= completed;
+            return avgTimeToComplete;
+        }
+
+        private static int IterateThroughRequests(Request request, ref int pending, ref int inProgress, ref double avgTimeToComplete,
+            ref int completed)
+        {
+            if (request.Status == RequestStatus.Pending)
+            {
+                IncreasePendingRequestCount(ref pending);
+            }
+                    
+            if (request.Status == RequestStatus.InProgress)
+            {
+                IncreaseInProgressRequestCount(ref inProgress);
+            }
+
+            if (request.Status == RequestStatus.Completed)
+            {
+                IncreaseCompletedRequestsCount(ref completed);
+                IncreaseSumOfTimeToComplete(request, ref avgTimeToComplete);
+            }
+
+            return pending;
+        }
+
+        private static void IncreaseSumOfTimeToComplete(Request request, ref double avgTimeToComplete)
+        {
+            avgTimeToComplete += (request.CompletionDate - request.StartingDate).Hours;
+        }
+
+        private static void IncreaseCompletedRequestsCount(ref int completed)
+        {
+            completed++;
+        }
+
+        private static void IncreaseInProgressRequestCount(ref int inProgress)
+        {
+            inProgress++;
+        }
+
+        private static void IncreasePendingRequestCount(ref int pending)
+        {
+            pending++;
         }
 
         private IEnumerable<(string, int, int, int, double)> BringReportTogether(List<(int, int, int, double)> requestTuples)
@@ -63,10 +102,15 @@ namespace BusinessLogic
 
             for (int i = 0; i < requestsFilter.Count; i++)
             {
-                report.Add((requestsFilter[i], requestTuples[i].Item1, requestTuples[i].Item2, requestTuples[i].Item3, requestTuples[i].Item4));
+                CreateReportTupleWithFilter(requestTuples, report, i);
             }
 
             return report;
+        }
+
+        private void CreateReportTupleWithFilter(List<(int, int, int, double)> requestTuples, List<(string, int, int, int, double)> report, int i)
+        {
+            report.Add((requestsFilter[i], requestTuples[i].Item1, requestTuples[i].Item2, requestTuples[i].Item3, requestTuples[i].Item4));
         }
     }
 }
