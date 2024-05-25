cccfb26c9f089f150400f71816f264db25cc6b03
jppuig <jppuigpesce@gmail.com>
2024-05-25 18:22:30 -0300

    Feat: LoginPage

---
+++ ClientApp/BuildingBossClientApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    LoadingSpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ClientApp/BuildingBossClientApp/src/app/auth/auth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+  isLoading: boolean = false;
+  error: string = "";
+
+  onSubmit(form: NgForm) {}
+}
