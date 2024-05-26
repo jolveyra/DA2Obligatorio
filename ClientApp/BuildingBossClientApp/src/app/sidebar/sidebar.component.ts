5b72013ebd68535117742dc312c0fdac7820cb2b
jppuig <jppuigpesce@gmail.com>
2024-05-26 19:25:29 -0300

    Feat: defined boilerplate for sidebar logic

--- ClientApp/BuildingBossClientApp/src/app/sidebar/sidebar.component.ts
+++ ClientApp/BuildingBossClientApp/src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
+  userRole: string = 'Administrator'; //FIXME: Change this to empty string
+  // userSub: Subscription;
+  
+  constructor(private authService: AuthService) { }
 
+  ngOnInit() {
+    // this.userSub = this.authService.user.subscribe(user => this.userRole = user.role);
+  }
+
+  ngOnDestroy() {
+    // this.userSub.unsubscribe();
+  }
 }
