e6fb6e58b73b0ceed241cb7b02238a0dcc14dcdd
Javier Olveyra <jolveyra@gmail.com>
2024-06-07 23:49:18 -0300

    Fix: JSONImporterFixes

--- Server/XMLImporterV1Test/XMLImporterV1Test.cs
+++ Server/XMLImporterV1Test/XMLImporterV1Test.cs
@@ -1,67 +1,68 @@
-//using XMLImporterV1;
-//using XMLImporterV1.Domain;
-//using System.Xml.Serialization;
-//using Moq;
+using XMLImporterV1;
+using XMLImporterV1.Domain;
+using BuildingImporter;
+using System.Xml.Serialization;
+using Moq;
 
-//namespace XMLImporterV1Test
-//{
-//    [TestClass]
-//    public class XMLImporterV1Test
-//    {
+namespace XMLImporterV1Test
+{
+    [TestClass]
+    public class XMLImporterV1Test
+    {
+        XMLImporter importer;
+        string path;
 
-//        private XMLImporter importer;
-//        string path;
+        [TestInitialize]
+        public void Initialize()
+        {
+            importer = new XMLImporter();
+            path = @"..\..\BuildingsFileStub.xml";
 
-//        [TestInitialize]
-//        public void Initialize()
-//        {
-//            path = @"..\..\buildings.xml";
+        }
 
-//        }
+        [TestMethod]
+        public void ImportBuildingsFromFileTestOk()
+        {
 
-//        [TestMethod]
-//        public void ImportBuildingsFromFileTestOk()
-//        {
+            List<DTOBuilding> buildings = new List<DTOBuilding>();
+            List<Edificio> edificios = new List<Edificio>() {
+                new Edificio()
+                {
+                    Nombre = "Las torres",
+                    Direccion = new Direccion
+                    {
+                        CallePrincipal = "Av. 6 de Diciembre",
+                        NumeroPuerta = 3030,
+                        CalleSecundaria = "Av. Eloy Alfaro"
+                    },
+                    Encargado = "unjuan.perez@gmail.com",
+                    Gps = new Gps
+                    {
+                        Latitud = -0.176,
+                        Longitud = -78.48
+                    },
+                    GastosComunes = 5000f,
+                    Departamentos = new List<Departamento>
+                    {
+                        new Departamento
+                        {
+                            Piso = 1,
+                            NumeroPuerta = 101,
+                            Habitaciones = 3,
+                            ConTerraza = false,
+                            Banos = 2,
+                            PropietarioEmail = "juan.perez@gmail.com"
+                        }
+                    }
+                }
+            };
 
-//            List<DTOBuilding> buildings = new List<DTOBuilding>();
-//            List<Edificio> edificios = new List<Edificio>() {  
-//                new Edificio() 
-//                {
-//                    Nombre = "Las torres",
-//                    Direccion = new Direccion
-//                    {
-//                        CallePrincipal = "Av. 6 de Diciembre",
-//                        NumeroPuerta = 3030,
-//                        CalleSecundaria = "Av. Eloy Alfaro"
-//                    },
-//                    Encargado = "unjuan.perez@gmail.com",
-//                    Gps = new Gps
-//                    {
-//                        Latitud = -0.176,
-//                        Longitud = -78.48
-//                    },
-//                    GastosComunes = 5000f,
-//                    Departamentos = new List<Departamento>
-//                    {
-//                        new Departamento
-//                        {
-//                            Piso = 1,
-//                            NumeroPuerta = 101,
-//                            Habitaciones = 3,
-//                            ConTerraza = false,
-//                            Banos = 2,
-//                            PropietarioEmail = "juan.perez@gmail.com"
-//                        }
-//                    }
-//                } 
-//            };
+            Root rootBuildingData = new Root() { Edificios = edificios };
 
-//            Root rootBuildingData = new Root() { Edificios = edificios };
+            buildings = importer.ImportBuildingsFromFile(path);
 
-//            buildings = importer.ImportBuildingsFromFile(path);
+            Assert.AreEqual(rootBuildingData.Edificios[0].Nombre, buildings[0].Name);
 
-//            Assert.AreEqual(rootBuildingData.Edificios[0].Nombre, buildings[0].Name);
-
-//        }
-//    }
-//}
+        }
+    }
+}
