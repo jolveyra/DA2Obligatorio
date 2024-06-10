c0d421502ab062181814bafaf8669684a9597573
Javier Olveyra <jolveyra@gmail.com>
2024-06-10 08:40:54 -0300

    Fix: JSONImporter Fix

--- Server/JSONImporterV1/Deserializer/JSONDeserializer.cs
+++ Server/JSONImporterV1/Deserializer/JSONDeserializer.cs
@@ -1,19 +1,16 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace JSONImporterV1.Deserializer
 {
-    public class JSONDeserializer: IJSONDeserializer
+    public class JSONDeserializer : IJSONDeserializer
     {
         public T Deserialize<T>(string path)
         {
             string json = File.ReadAllText(path);
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonSerializer.Deserialize<T>(json);
         }
     }
 }
